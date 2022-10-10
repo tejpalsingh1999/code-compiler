8e71314e8d2beb3d30a75e21fba15f3299b4afda
Tejpal Singh <tejpalsingh191.official@gmail.com>
2022-10-10 19:40:06 +0530

    basic project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Global from "./styles/Global";
+
+function App() {
+  return (
+    <div>
+      <Global />
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/Global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        pading: 0;
+        outline: 0;
+        box-sizing: border-box;
+        font-family: 'Play', sans-serif;
+    }
+`;
