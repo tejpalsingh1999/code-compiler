6c5aa01bb13bf0e011829fe648ac3380b1585c36
Tejpal Singh <tejpalsingh191.official@gmail.com>
2022-10-23 12:21:58 +0530

    UI Fix

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { RiCloseFill } from "react-icons/ri";
 
 const ModalContainer = styled.div`
   background: rgba(0, 0, 0, 0.5);
@@ -17,7 +18,7 @@ const ModalContainer = styled.div`
 
 const ModalContent = styled.div`
   background: white;
-  width: 30%;
+  width: 35%;
   padding: 2rem;
   border-radius: 10px;
 `;
@@ -28,13 +29,23 @@ const Header = styled.div`
   align-items: center;
 `;
 
+const CloseButton = styled.button`
+  background: transparent;
+  outline: 0;
+  border: 0;
+  font-size: 2rem;
+  cursor: pointer;
+`;
+
 function Modal() {
   return (
     <ModalContainer>
       <ModalContent>
         <Header>
           <h2 className="heading">Update Folder Name</h2>
-          <button>Close Button</button>
+          <CloseButton>
+            <RiCloseFill />
+          </CloseButton>
         </Header>
       </ModalContent>
     </ModalContainer>
